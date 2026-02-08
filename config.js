4362ac49e92e2867518aa37c2786ce60e79ab6da
Jean Almeida <156534978+jeandevzinweb@users.noreply.github.com>
2026-02-07 21:14:54 -0300

    Add portfolio data configuration

---
+++ config.js
@@ -0,0 +1,43 @@
+const portfolioData = {
+    courses: [
+        {
+            name: "Design De Interface Web",
+            institution: "SENAI",
+            year: 2025
+        },
+        {
+            name: "Design UX e UI",
+            institution: "SENAI",
+            year: 2025
+        },
+        {
+            name: "Fluência - Fundamentos da IA",
+            institution: "SENAI",
+            year: 2025
+        },
+        {
+            name: "Trilha Full Stack JS",
+            institution: "OneBitCode",
+            year: 2024
+        },
+        {
+            name: "Lógica de Programação",
+            institution: "Curso em Video (Gustavo Guanabara)",
+            year: 2023
+        }
+    ],
+    projects: [
+        {
+            title: "Dashboard Para Gerenciamento Financeiro",
+            image: "projeto1.png",
+            description: "Tenha em mãos todos os seus gastos de forma visivel e mais detalhado!",
+            icon: "fa-solid fa-laptop-code"
+        },
+        {
+            title: "Portifólio Pessoal",
+            image: "projeto2.png",
+            description: "Um portfólio pessoal para mostrar minhas habilidades e projetos.",
+            icon: "fa-solid fa-laptop-code"
+        }
+    ]
+};
